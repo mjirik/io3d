2c1f2604320b7ba3a3f213c305c30eab7a1df6ad
M. Jirik <miroslav.jirik@gmail.com>
2014-10-29 11:19:43 +0100

    tests working

--- io3d/__init__.py
+++ io3d/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['pycut', 'seed_editor_qt', 'dcmreaddata']
-from pycut import Model, ImageGraphCut
-from seed_editor_qt import QTSeedEditor
+__all__ = ['dcmreaddata', 'datareader', 'datawriter']
+# from pycut import Model, ImageGraphCut
+# from seed_editor_qt import QTSeedEditor
 from dcmreaddata import DicomReader
