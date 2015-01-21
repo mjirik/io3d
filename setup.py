c4f95aee40660dec26d7e53e5cb86975a432d9a6
M. Jirik <miroslav.jirik@gmail.com>
2015-01-21 10:32:18 +0100

    SimpleITK added to requirements

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['numpy', 'scipy', 'pydicom'],
+    install_requires=['numpy', 'scipy', 'pydicom', 'SimpleITK'],
     dependency_links=[],
 
     # If there are data files included in your packages that need to be
