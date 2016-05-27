8106248dbed042b26276088227dbe4a235a0a26c
M. Jirik <miroslav.jirik@gmail.com>
2016-05-27 19:17:35 +0200

    version in init

--- io3d/__init__.py
+++ io3d/__init__.py
@@ -1,4 +1,5 @@
 __all__ = ['dcmreaddata', 'datareader', 'datawriter', 'rawN']
+__version__ = "1.0.31"
 # from pycut import Model, ImageGraphCut
 # from seed_editor_qt import QTSeedEditor
 from dcmreaddata import DicomReader
---
+++ io3d/connector.py
@@ -0,0 +1,48 @@
+#  /usr/bin/python
+# -*- coding: utf-8 -*-
+""" Module for readin 3D dicom data
+"""
+
+# import funkcí z jiného adresáře
+import sys
+import os.path
+
+# path_to_script = os.path.dirname(os.path.abspath(__file__))
+# sys.path.append(os.path.join(path_to_script, "../extern/pyseg_base/src"))
+# sys.path.append(os.path.join(path_to_script,
+#                              "../extern/py3DSeedEditor/"))
+# ys.path.append(os.path.join(path_to_script, "../extern/"))
+# mport featurevector
+
+import logging
+logger = logging.getLogger(__name__)
+import argparse
+
+__author__ = 'mjirik'
+
+import  paramiko
+
+host = "147.228.47.162"                    #hard-coded
+port = 22
+transport = paramiko.Transport((host, port))
+
+username = "lisa_sftp"                #hard-coded
+username = "lisa_normal"                #hard-coded
+password = "L154.sftp"                #hard-coded
+# username = "mjirik"                #hard-coded
+# password = "titanic"                #hard-coded
+transport.connect(username = username, password = password)
+
+sftp = paramiko.SFTPClient.from_transport(transport)
+
+import sys
+path = './' + sys.argv[1]    #hard-coded
+localpath = sys.argv[1]
+sftp.put(localpath, path)
+
+sftp.close()
+transport.close()
+print 'Upload done.'
+
+
+
