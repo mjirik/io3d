c7635bf04fc3868d4006b128061356686eb34b0b
Miroslav Jirik <miroslav.jirik@gmail.com>
2017-11-01 16:54:05 +0100

    dataset tests

---
+++ tests/datasets_test.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 mjirik <mjirik@mjirik-Latitude-E6520>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Module for testing format rawiv
+"""
+import logging
+logger = logging.getLogger(__name__)
+import unittest
+from nose.plugins.attrib import attr
+import numpy as np
+import os
+import os.path as op
+import shutil
+
+import io3d
+
+
+class DatasetsTest(unittest.TestCase):
+
+    # @attr('actual')
+    # @attr('interactive')
+    def test_download(self):
+
+        io3d.datasets.download("gensei_slices")
+        pth = op.join(io3d.datasets.local_dir, "gensei_slices")
+        self.assertTrue(op.exists(pth))
+        # import sed3
+        # ed = sed3.sed3(data3d)
+        # ed.show()
+        # on this index sould be number 119
+        # self.assertEqual(data3d[29, 13, 27], 119)
+        pass
+
+    def test_download_to_dir(self):
+        pth = op.join("./tmp/", "biodur_sample")
+        if op.exists(pth):
+            shutil.rmtree(pth)
+
+        io3d.datasets.download("biodur_sample", "./tmp/")
+        pth = op.join("./tmp/", "biodur_sample")
+        self.assertTrue(op.exists(pth))
+
+if __name__ == "__main__":
+    unittest.main()
