f590bb41efc61303b60339389d38eb9c5c5452ad
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-02-14 20:46:44 +0100

    get output path widget

---
+++ io3d/outputqt.py
@@ -0,0 +1,107 @@
+import logging
+
+logger = logging.getLogger(__name__)
+import argparse
+from PyQt4.QtGui import QGridLayout, QLabel, QPushButton, QLineEdit, QCheckBox, QFileDialog
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+import sys
+import numpy as np
+import os.path as op
+
+import vtk
+from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+
+class SelectOutputPathWidget(QtGui.QWidget):
+    def __init__(self, path=None, widget_label=None, save_dialog_message="Save file",
+                 save_dialog_filter="", *args, **kwargs):
+        super(SelectOutputPathWidget, self).__init__()
+
+        self.ui_slab = {}
+        self.output_path = ""
+
+        self.mainLayout = QGridLayout(self)
+        # self.init_slab(*args, **kwargs)
+        self.save_dialog_message = save_dialog_message
+        self.save_dialog_filter = save_dialog_filter
+        self.widget_label = widget_label
+        self.init_ui()
+
+        if path is not None:
+            self.set_path(path)
+
+    def set_path(self, path):
+        dirname, filename = op.split(path)
+        self.output_path = path
+
+        self.ui_buttons["dirname"].setText(dirname)
+        self.ui_buttons["filename"].setText(filename)
+
+        # self._filename = filename
+        # self._dirname = dirname
+
+    def get_dirname(self):
+        dirname = str(self.ui_buttons["dirname"].text())
+        return dirname
+
+    def get_filename(self):
+        filename = str(self.ui_buttons["filename"].text())
+        return filename
+
+    def get_path(self):
+        dirname = self.get_dirname()
+        filename = self.get_filename()
+        path = op.join(dirname, filename)
+        return path
+
+    def action_select_path(self):
+        pth = op.expanduser(self.get_path())
+        self.set_path(str(QFileDialog.getSaveFileName(
+            self,
+            self.save_dialog_message,
+            pth,
+            filter=self.save_dialog_filter
+        )))
+
+
+    # def update_ui(self):
+    #     keyword = "dirname"
+    #     self.ui_buttons[keyword].setText(str(self._dirname))
+    #     keyword = "filename"
+    #     self.ui_buttons[keyword].setText(str(self._filename))
+
+
+    def init_ui(self):
+
+        # self.mainLayout = QGridLayout(self)
+
+        self._row = 0
+        self.ui_buttons = {}
+
+        self._row += 1
+        if self.widget_label is not None:
+            keyword = "label"
+            vtk_fileQLabel= QLabel(self.widget_label)
+            self.mainLayout.addWidget(vtk_fileQLabel, self._row, 2)
+            print("-----------------")
+
+
+        keyword = "dirname"
+        self.ui_buttons[keyword] = QLineEdit()
+        # self.ui_buttons[keyword].setText(str(self.output_path))
+        self.mainLayout.addWidget(self.ui_buttons[keyword], self._row + 1, 2, 1, 2)
+        vtk_fileQLabel= QLabel("dir")
+        self.mainLayout.addWidget(vtk_fileQLabel, self._row + 1, 1)
+        keyword = "filename"
+        self.ui_buttons[keyword] = QLineEdit()
+        # self.ui_buttons[keyword].setText(str(self.output_path))
+        self.mainLayout.addWidget(self.ui_buttons[keyword], self._row + 2, 2)
+        vtk_fileQLabel= QLabel("file")
+        self.mainLayout.addWidget(vtk_fileQLabel, self._row + 2, 1)
+        keyword = "path_button"
+        self.ui_buttons[keyword] = QPushButton("Select", self)
+        self.ui_buttons[keyword].clicked.connect(self.action_select_path)
+        self.mainLayout.addWidget(self.ui_buttons[keyword], self._row + 2, 3, 1, 1)
+
+        # self.update_ui()
+
---
+++ tests/qt_test.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import logging
+logger = logging.getLogger(__name__)
+# import funkcí z jiného adresáře
+# import sys
+import os.path
+import os.path as op
+# import copy
+
+import unittest
+from nose.plugins.attrib import attr
+# sample_data_path = os.path.dirname(os.path.abspath(__file__))
+# sample_data_path
+# sys.path.append(os.path.join(path_to_script, "../extern/pyseg_base/src/"))
+# sys.path.append(os.path.join(path_to_script, "../extern/py3DSeedEditor/"))
+# sys.path.append(os.path.join(path_to_script, "../src/"))
+
+# from PyQt4.QtGui import QFileDialog, QApplication, QMainWindow
+
+import numpy as np
+
+import dicom
+dicom.debug(False)
+
+#
+import sys
+import io3d
+import io3d.dcmreaddata as dcmr
+# sample_data_path = "~/data/medical/orig/sample_data/"
+# sample_data_path = op.expanduser(sample_data_path)
+
+from PyQt4.QtGui import QApplication
+import io3d.outputqt
+
+class QtTest(unittest.TestCase):
+    interactivetTest = False
+
+    # def setUp(self):
+    #     import imtools
+    #     import imtools.sample_data
+    #     imtools.sample_data.get_sample_data(["jatra_5mm", "volumetrie"], SAMPLE_DATA_DIR)
+#    def setUp(self):
+#        self.dcmdir = os.path.join(path_to_script, '../sample_data/jatra_5mm')
+# self.data3d, self.metadata = dcmr.dcm_read_from_dir(self.dcmdir)
+#        reader = dcmr.DicomReader(self.dcmdir)
+#        self.data3d = reader.get_3Ddata()
+#        self.metadata = reader.get_metaData()
+    def test_select_ouput_path(self):
+        app = QApplication(sys.argv)
+        sopw = io3d.outputqt.SelectOutputPathWidget(widget_label="widget label", path="~/lisa_data/sample.{}.pkl")
+        sopw.show()
+
+        in_path = "~/sample{}.vtk"
+        sopw.set_path(in_path)
+
+        out_path = sopw.get_path()
+        home = op.expanduser("~")
+
+        rp1 = op.relpath(home, in_path)
+        rp2 = op.relpath(home, out_path)
+        # app.exec_()
+        self.assertEqual(rp1, rp2)
+
+    def test_select_ouput_filename(self):
+        app = QApplication(sys.argv)
+        sopw = io3d.outputqt.SelectOutputPathWidget(widget_label="widget label", path="~/lisa_data/sample.{}.pkl")
+        sopw.show()
+
+        in_path = "~/sample{}.vtk"
+        sopw.set_path(in_path)
+
+        out_path = sopw.get_filename()
+        # app.exec_()
+        self.assertEqual(out_path, "sample{}.vtk")
+
+if __name__ == "__main__":
+    unittest.main()
