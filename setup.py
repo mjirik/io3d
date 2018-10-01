ce6adc57c9a3b2d6504dd3519d984e9b6488917e
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-10-01 16:58:26 +0200

    Bump version: 1.5.1 → 1.5.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.5.1',
+    version='1.5.2',
     url='https://github.com/mjirik/io3d',
     author='Miroslav Jirik',
     author_email='miroslav.jirik@gmail.com',
