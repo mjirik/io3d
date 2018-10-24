dc8b3a42418edc78822de301a78cad1cacdf55b4
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-10-24 12:44:45 +0200

    anonymisation test

---
+++ tests/anon_test.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+import logging
+logger = logging.getLogger(__name__)
+
+import unittest
+import os.path as op
+import os
+import io3d
+import io3d.anonym
+
+
+class AnonTestCase(unittest.TestCase):
+
+    @unittest.skipIf(os.environ.get("TRAVIS", True), "Skip on Travis-CI")
+    def test_anon_file(self):
+        output_file = 'output_anon.dcm'
+        if op.exists(output_file):
+            os.remove(output_file)
+        anon = io3d.anonym.Anonymizer()
+        cesta_k_souboru_jatrer = io3d.datasets.join_path("medical", "orig", "jatra_5mm", "IM-0001-0001.dcm", get_root=True)
+        anon.file_anonymization(cesta_k_souboru_jatrer, output_file)
+        self.assertTrue(op.exists(output_file))
+
+
+if __name__ == '__main__':
+    unittest.main()
