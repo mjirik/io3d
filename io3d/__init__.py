c384eb125fafdc7865b2eacecd085b7426ecbad1
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-10-03 19:55:59 +0200

    Bump version: 2.2.0 → 2.2.1

--- io3d/__init__.py
+++ io3d/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["dcmreaddata", "datareader", "datawriter", "rawN", "datasets"]
-__version__ = "2.2.0"
+__version__ = "2.2.1"
 # from pycut import Model, ImageGraphCut
 # from seed_editor_qt import QTSeedEditor
 from .dcmreaddata import DicomReader
