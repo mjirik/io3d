0fc6cf9a84aaab2edaceb295f14696ebc415e81e
Miroslav Jirik <mjirik@ntis.zcu.cz>
2020-10-20 12:41:35 +0200

    Bump version: 2.5.4 → 2.5.5

--- io3d/__init__.py
+++ io3d/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["dcmreaddata", "datareader", "datawriter", "rawN", "datasets"]
-__version__ = "2.5.4"
+__version__ = "2.5.5"
 # from pycut import Model, ImageGraphCut
 # from seed_editor_qt import QTSeedEditor
 from loguru import logger
