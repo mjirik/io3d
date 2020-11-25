aff1f3d0df0e4473afffb65e9f58b95b8f658b49
Miroslav Jirik <miroslav.jirik@gmail.com>
2020-11-25 21:54:26 +0100

    we do not need simple itk 1.*

---
+++ devel/itkread.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import SimpleITK as sitk
+import numpy as np
+
+# pth = Path("~/data/medical/orig/")
+pth = r"G:\Můj disk\data\medical\orig\sliver07\training\liver-orig001.mhd"
+
+
+def readim(pth):
+    ppth = Path(pth)
+
+    print(ppth.exists())
+    im = sitk.ReadImage(pth)
+    imnp = sitk.GetArrayFromImage(im)
+
+    print(f"shape={imnp.shape}")
+    mx =np.max(imnp)
+    print(f"{mx=}")
+    print(f"{np.min(imnp)=}")
+
+
+pth1 = r"h:\medical\orig\sliver07\training\liver-orig001.mhd"
+pth2 = r"G:\Můj disk\data\medical\orig\sliver07\training\liver-orig001.mhd"
+
+readim(pth1)
+readim(pth2)
