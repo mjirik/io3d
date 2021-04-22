494e3c3d9bdca2b62c7445c2ab2d8a0fbe61ef7e
Miroslav Jiřík <miroslav.jirik@gmail.com>
2021-04-22 09:22:00 +0200

    create dicom.pkl

--- devel/create_dicomdir_pilsen_pigs.py
+++ devel/create_dicomdir_pilsen_pigs.py
@@ -6,12 +6,16 @@ logger.enable("io3d")
 base_path = Path(r'H:\biomedical\orig\pilsen_pigs_raw\transplantation')
 
 # fnlist = base_path.glob("*debug*")
-fnlist = base_path.glob("*Tx041D_V*")
+# fnlist = base_path.glob("*Tx041D_V*")
+fnlist = base_path.glob("*Tx0*D_V*")
 
 for fn in fnlist:
+    logger.debug(f"{fn}")
     if not (fn / "dicomdir.pkl").exists():
-        logger.debug(f"{fn}")
-        io3d.read(str(fn))
+        try:
+            io3d.read(str(fn))
+        except Exception as e:
+            logger.warning(e)
         # io3d.read(fn)
 
         logger.debug(f"{(fn /'dicomdir.pkl').exists()}")
