c0fca422e2a3ba36dcb612f02f2624473550a864
elina2004 <62164325+elina2004@users.noreply.github.com>
2021-04-27 10:03:14 +0200

    Update transform_file_structure.py
    Nahrani a vytvoreni struktury slozek

--- devel/dataset/transform_file_structure.py
+++ devel/dataset/transform_file_structure.py
@@ -12,6 +12,56 @@ def prepare_pilsen_pigs_dataset(input_dir, output_dir, output_format=".mhd"):
     dp = io3d.read(list(imput_dir.glob('*040D_V*'))[0])
     f_name = list(imput_dir.glob('*040D_V*'))[0]
 
+    output_dir = Path(r'G:\.shortcut-targets-by-id\1nz7i4Zssar8075yu6J5QpHdph6oi3uF8\pilsen_pigs')
+    io3d.write({'data3d': np.zeros([10, 10, 10])}, output_dir / 'pokus.pklz')
+
+    # ukladani obsahu slozek
+    k = 0
+
+    for f_name in file_list:
+        p = str(r'C:\Users\janar\data\biomedical\orig\Pilsen_pigs\pig_')
+        p = p + str(k)
+        print(p)
+
+        p1 = p + '\LABELED_DICOM'
+        p2 = p + '\MASKS_DICOM'
+        p3 = p + '\MESHES_VTK'
+        p4 = p + '\PATIENT_DICOM'
+
+        p1 = Path(p1)
+        p1.mkdir(parents=True, exist_ok=True)
+
+        dp = io3d.read(f_name)
+        output_f = p1 / (f_name.stem + '.mhd')
+        io3d.write(dp, output_f)
+
+        p2 = Path(p2)
+        p2.mkdir(parents=True, exist_ok=True)
+
+        dp = io3d.read(f_name)
+        output_f = p2 / (f_name.stem + '.mhd')
+        io3d.write(dp, output_f)
+
+        p3 = Path(p3)
+        p3.mkdir(parents=True, exist_ok=True)
+
+        dp = io3d.read(f_name)
+        output_f = p3 / (f_name.stem + '.mhd')
+        io3d.write(dp, output_f)
+
+        p4 = Path(p4)
+        p4.mkdir(parents=True, exist_ok=True)
+
+        dp = io3d.read(f_name)
+        output_f = p4 / (f_name.stem + '.mhd')
+        io3d.write(dp, output_f)
+
+        #     p4 = Path(p4)
+        #     p4.parent.mkdir(parents=True, exist_ok=True)
+
+        k = k + 1
+    print(k)
+
     pass
 
 
