701a309650b5ea8d61235fb1045e60a29120ccb1
elina2004 <62164325+elina2004@users.noreply.github.com>
2021-04-27 09:15:30 +0200

    Update transform_file_structure.py
    Import and donwland of a file V_40

--- devel/dataset/transform_file_structure.py
+++ devel/dataset/transform_file_structure.py
@@ -2,7 +2,15 @@
 
 
 def prepare_pilsen_pigs_dataset(input_dir, output_dir, output_format=".mhd"):
+    from google_drive_downloader import GoogleDriveDownloader as gdd
+    import io3d
+    from pathlib import Path
     import SimpleITK
+
+    # teoretické načtení složky T040
+    imput_dir = Path(r'G:\.shortcut-targets-by-id\1Vq6QAiJ7cHDJUdgKXjUxx5iw7zk6MMPI\transplantation')
+    dp = io3d.read(list(imput_dir.glob('*040D_V*'))[0])
+    f_name = list(imput_dir.glob('*040D_V*'))[0]
 
     pass
 
