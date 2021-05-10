ccc857a3d4842331a2cd93d4a71ad8993b312035
Miroslav Jiřík <miroslav.jirik@gmail.com>
2021-05-10 21:07:36 +0200

    create dataset into any directory

--- devel/dataset/transform_file_structure.py
+++ devel/dataset/transform_file_structure.py
@@ -1,22 +1,28 @@
+import io3d
+from pathlib import Path
+import SimpleITK
+import regex
+from loguru import logger
 
 
-
-def prepare_pilsen_pigs_dataset(input_dir, output_dir, input_mask='*_Ven*', start_index = 0, output_format=".mhd"):
+def prepare_pilsen_pigs_dataset(input_dir, output_dir, input_mask='*_Ven*', start_index = 0, output_format=".mhd", dirname_pattern='pig_{i:04d}', regexp_pattern="Tx([0-9]+)"):
     """
-    input_dir: vstupni cesta
-    output_dir: vystup. cesta
-    input_mask: cast nazvu zvol. souboru (pro fci. glob)
-    start_index: cislo od ktereho zacneme vytvaret slozky
+    :param input_dir: vstupni cesta
+    :param output_dir: vystup. cesta
+    :param input_mask: cast nazvu zvol. souboru (pro fci. glob)
+    :param start_index: cislo od ktereho zacneme vytvaret slozky
+    :param dirname_pattern: pattern used for construction of output directory name, `i` or `re_out` can be used. I.e. 'pig_{re_out[0]}', or '{re_out[0]}
+    :param regexp_pattern: used to extract regex_i from filename. I.e. '(Tx[0-9]+D_Ven)'
     """
 
-    import io3d
-    from pathlib import Path
-    import SimpleITK
 
     # ukladani obsahu slozek
     file_list = list(input_dir.glob(input_mask))
+    i = start_index
     for f_name in file_list:
-        p = output_dir/ f'pig_{start_index:04d}'
+        re_out = regex.findall(regexp_pattern, str(f_name))
+        regex_i = re_out[0] if len(re_out) else None
+        p = output_dir/ dirname_pattern.format(i=i, regex_i=regex_i, re_out=re_out)
 
 
         p1 = p /'LABELED_DICOM'
@@ -27,35 +33,36 @@ def prepare_pilsen_pigs_dataset(input_dir, output_dir, input_mask='*_Ven*', star
         p1 = Path(p1)
         p1.mkdir(parents=True, exist_ok=True)
 
-        dp = io3d.read(f_name)
+        # dp = io3d.read(f_name)
         output_f = p1 / (f_name.stem + output_format)
-        io3d.write(dp, output_f)
+        # io3d.write(dp, output_f)
 
         p2 = Path(p2)
         p2.mkdir(parents=True, exist_ok=True)
 
-        dp = io3d.read(f_name)
+        # dp = io3d.read(f_name)
         output_f = p2 / (f_name.stem + output_format)
-        io3d.write(dp, output_f)
+        # io3d.write(dp, output_f)
 
         p3 = Path(p3)
         p3.mkdir(parents=True, exist_ok=True)
 
-        dp = io3d.read(f_name)
+        # dp = io3d.read(f_name)
         output_f = p3 / (f_name.stem + output_format)
-        io3d.write(dp, output_f)
+        # io3d.write(dp, output_f)
 
         p4 = Path(p4)
         p4.mkdir(parents=True, exist_ok=True)
 
         dp = io3d.read(f_name)
         output_f = p4 / (f_name.stem + output_format)
+        logger.debug(f"writiong into {output_f}")
         io3d.write(dp, output_f)
 
         #     p4 = Path(p4)
         #     p4.parent.mkdir(parents=True, exist_ok=True)
 
-        start_index = start_index + 1
+        i = i + 1
 
     pass
 
--- devel/dataset/transform_file_structure_test.py
+++ devel/dataset/transform_file_structure_test.py
@@ -4,6 +4,7 @@ import unittest
 from pathlib import Path
 from devel.dataset import transform_file_structure
 import io3d
+from loguru import logger
 
 
 @unittest.skipIf(os.environ.get("TRAVIS", False), "Skip on Travis-CI")
@@ -15,6 +16,31 @@ def test_pilsen_pigs():
     output_dir = Path(r'G:\.shortcut-targets-by-id\1nz7i4Zssar8075yu6J5QpHdph6oi3uF8\pilsen_pigs')
     file_list1 = list(input_dir.glob('*_Ven*'))
     transform_file_structure.prepare_pilsen_pigs_dataset(input_dir, output_dir=output_dir, output_format=".mhd")
+    file_list2 = list(input_dir.glob('*_Ven*'))
+
+    # assert (output_dir / "pilsen_pigs_Tx041D_Ven" / "PATIENT_DICOM" / "pig_001.mhd").exists()
+    # assert (output_dir / "pilsen_pigs_Tx041D_Ven" / "PATIENT_DICOM" / "pilsen_pigs_Tx041D_Ven.raw").exists()
+
+    assert (len(file_list1) < len(file_list2))
+
+
+@unittest.skipIf(os.environ.get("TRAVIS", False), "Skip on Travis-CI")
+def test_pilsen_pigs_create_in_dataset_dir():
+    input_mask="*Tx027D_Ven*"
+    input_dir = io3d.joinp("biomedical/orig/pilsen_pigs_raw/transplantation")   # r'H:\biomedical\orig\pilsen_pigs_raw\transplantation'
+    data_id = 1
+    organ_key = "Left Kidney"
+    output_type = ".png"
+    organ_key_file = organ_key.lower().replace(" ", "_")
+    output_dir = io3d.datasets.get_dataset_path("pilsen_pigs", organ_key_file, data_id=data_id).parents[2]
+    # input_dir = r'H:\biomedical\orig\pilsen_pigs_raw\transplantation'
+    # output_dir = Path(r"H:\biomedical\orig\pilsen_pigs")
+    assert input_dir.exists()
+    # input_dir = Path(r'G:\.shortcut-targets-by-id\1Vq6QAiJ7cHDJUdgKXjUxx5iw7zk6MMPI\transplantation')
+    # output_dir = Path(r'G:\.shortcut-targets-by-id\1nz7i4Zssar8075yu6J5QpHdph6oi3uF8\pilsen_pigs')
+    file_list1 = list(input_dir.glob('*_Ven*'))
+    logger.debug(output_dir)
+    transform_file_structure.prepare_pilsen_pigs_dataset(input_dir, output_dir=output_dir,input_mask=input_mask, output_format=".mhd", dirname_pattern='{re_out[0]}', regexp_pattern='(Tx[0-9]+D_Ven)')
     file_list2 = list(input_dir.glob('*_Ven*'))
 
     # assert (output_dir / "pilsen_pigs_Tx041D_Ven" / "PATIENT_DICOM" / "pig_001.mhd").exists()
